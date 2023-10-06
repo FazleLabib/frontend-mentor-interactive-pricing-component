0afd2064191c30aff80f2ea74dc46824b6eb4361
Falze Labib <fazle.labib@gmail.com>
2023-10-07 00:13:02 +0600

    Implement refactoring

--- script.js
+++ script.js
@@ -5,55 +5,42 @@ const billingYear = document.getElementById('year');
 const billingMonth = document.getElementById('month');
 
 const slider = document.getElementById('slider-range');
-
 const toggle = document.getElementById('toggle-input');
 
-let pricingValue = 16;
 const discount = 25;
 
+const pricingData = {
+  1: { views: "10K", monthly: 8},
+  2: { views: "50K", monthly: 12},
+  3: { views: "100K", monthly: 16},
+  4: { views: "500K", monthly: 24},
+  5: { views: "1M", monthly: 36},
+};
+
 function checkToggle() {
-    setPrice();
+  setPrice();
 }
 
 function setPrice() {
-    slider.style.setProperty('--slider-value', slider.value);
-    if (slider.value === "1") {
-        views.innerHTML = "10K";
-        pricingValue = getPrice(toggle.checked, 8);
-        pricing.innerHTML = `$${pricingValue.toFixed(2)}`;
-    } else if (slider.value === "2") {
-        views.innerHTML = "50K";
-        pricingValue = getPrice(toggle.checked, 12);
-        pricing.innerHTML = `$${pricingValue.toFixed(2)}`;
-    } else if (slider.value === "3") {
-        views.innerHTML = "100K";
-        pricingValue = getPrice(toggle.checked, 16);
-        pricing.innerHTML = `$${pricingValue.toFixed(2)}`;
-    } else if ((slider.value === "4")) {
-        views.innerHTML = "500K";
-        pricingValue = getPrice(toggle.checked, 24);
-        pricing.innerHTML = `$${pricingValue.toFixed(2)}`;
-    } else {
-        views.innerHTML = "1M";
-        pricingValue = getPrice(toggle.checked, 36);
-        pricing.innerHTML = `$${pricingValue.toFixed(2)}`;
-    }
+
+  const sliderValue = parseInt(slider.value);
+  const data = pricingData[sliderValue];
+
+  slider.style.setProperty('--slider-value', sliderValue);
+  views.innerHTML = data.views;
+  pricingValue = getPrice(toggle.checked, data.monthly);
+  pricing.innerHTML = `$${pricingValue.toFixed(2)}`;
+
 }
 
 function getPrice(checked, pricingValue) {
 
-    if (checked) {
-        pricingValue = pricingValue - (pricingValue * discount/100);
-        billingYear.style.display = 'block';
-        billingMonth.style.display = 'none';
-    } else {
-        billingYear.style.display = 'none';
-        billingMonth.style.display = 'block';
-    }
-    return pricingValue
+  const discountedPrice = checked ? pricingValue * (1 - discount / 100) : pricingValue;
+  billingYear.style.display = checked ? 'block' : 'none';
+  billingMonth.style.display = checked ? 'none' : 'block';
+  return discountedPrice;
 
 }
 
 slider.addEventListener('input', setPrice);
 toggle.addEventListener('change', checkToggle);
-
